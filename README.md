a6c1321615c9b302db5196be7a1d25b35cf15f05
J-Galvis <galviscamilojuan@gmail.com>
2024-07-31 19:01:10 -0500

    Class for the board squares

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Checkers Project
+
+CHeckers game that implements various types of IA.
+
+Using the library imparaai-checkers: https://github.com/ImparaAI/checkers
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from setup import *
 
 pygame.init()
 
@@ -12,26 +13,43 @@ SQUARE_SIZE = WIDTH // COLS
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+class Rectangle:
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x*SQUARE_SIZE, y*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE)
+        self.x = x
+        self.y = y
+        self.number = None
+        self.Playable = False if (self.x + self.y) % 2 == 0 else True
+        self.color = BLACK if self.Playable else WHITE
+
+        if self.Playable:
+            if self.y == 0: self.number = (self.x + 1)//2
+            else: self.number = ((self.y * 4) + (self.x)//2) +1
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+
+    def is_clicked(self, pos):
+        return self.rect.collidepoint(pos)
+
 # Create the screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers Board')
 
-def draw_board(screen)-> None:
-    counter=0
+def create_board()-> None:
     for row in range(ROWS):
         for col in range(COLS):
-            color = WHITE if (row + col) % 2 == 0 else BLACK
-            pygame.draw.rect(screen, color, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-            listPositions.append([counter,row,col])
+            listPositions.append(Rectangle(col, row))
 
-def verPosiciones(pos: tuple[int,int])-> int:
-    ...
-
+def draw_board(screen)-> None:
+    for position in listPositions:
+        position.draw(screen)
 
 
 def initGame() -> None:
     clock = pygame.time.Clock()
     clock.tick(60)
+    create_board()
 
     run = True
 
@@ -41,7 +59,9 @@ def initGame() -> None:
                 run = False
             
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
+                for position in listPositions:
+                    if position.is_clicked(pygame.mouse.get_pos()):
+                        print(position.number)
 
         draw_board(screen)
         pygame.display.flip()
